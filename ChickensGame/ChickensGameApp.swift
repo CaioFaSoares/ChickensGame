7374221c11ce1f810a13f16c4eeb519fccf91175
Caio <caiofasoares@gmail.com>
2023-03-28 11:29:06 -0300

    Initial Commit

---
+++ ChickensGame/ChickensGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChickensGameApp.swift
+//  ChickensGame
+//
+//  Created by Caio Soares on 28/03/23.
+//
+
+import SwiftUI
+
+@main
+struct ChickensGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
