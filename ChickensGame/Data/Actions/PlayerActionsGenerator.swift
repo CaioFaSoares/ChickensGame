2f64c3fe9deb6cc97051ca2aa2678328232fbf8e
Lucas Barbosa de Oliveira <lucasbarbosa2807@gmail.com>
2023-04-08 18:48:10 -0300

    alteration button in mainscreen

--- ChickensGame/Data/Actions/PlayerActionsGenerator.swift
+++ ChickensGame/Data/Actions/PlayerActionsGenerator.swift
@@ -12,14 +12,19 @@ class PlayerAttackGenerator {
         return EntityAction(isHealing: false,
                             value: 3, cooldown: 0, accuracy: 0.90,
                             internalID: 0001, internalName: "player_basicAttack_0001",
-                            contextualName: "Basic Attack!")
+                            contextualName: "Basic \nAttack!")
     }
     
     func generateStrongAttack() -> EntityAction {
         return EntityAction(isHealing: false,
                             value: 6, cooldown: 1, accuracy: 0.70,
                             internalID: 0002, internalName: "player_strongAttack_0002",
-                            contextualName: "Strong Attack!")
+                            contextualName: "Strong \nAttack!")
     }
-
+    func generateSkillHealing() -> EntityAction {
+        return EntityAction(isHealing: true,
+                            value: 5, cooldown: 2, accuracy: 1,
+                            internalID: 0003, internalName: "player_strongAttack_0003",
+                            contextualName: "Healing \nCorn")
+    }
 }
