58341a838f1ad5a46fa0d95f9ee91a0f7a0c2f64
Lucas Barbosa de Oliveira <lucasbarbosa2807@gmail.com>
2023-04-10 11:09:48 -0300

    Animation
    created a function to animate the sprites

--- ChickensGame/Scenes/Combat/CombatScene.swift
+++ ChickensGame/Scenes/Combat/CombatScene.swift
@@ -12,19 +12,36 @@ class CombatScene: SKScene, ObservableObject {
 	
 	let defaultScale = CGSize(width: UIScreen.main.bounds.width / 6, height:  UIScreen.main.bounds.height / 6 / 2)
 	
-	let playerXSP = UIScreen.main.bounds.width / 6
-	let playerYSP = UIScreen.main.bounds.width / 6
+	let playerXSP = UIScreen.main.bounds.width / 4
+	let playerYSP = UIScreen.main.bounds.width / 4
 	
 	override func didMove(to view: SKView) {
 		physicsBody = SKPhysicsBody(edgeLoopFrom: frame)
+
 	}
+
+    func createAnimation(texturePrefix: String, frameCount: Int, timePerFrame: TimeInterval) -> SKAction {
+        var frames: [SKTexture] = []
+        for i in 0..<frameCount {
+            let textureName = "\(texturePrefix)\(i)"
+            let texture = SKTexture(imageNamed: textureName)
+            frames.append(texture)
+        }
+        return SKAction.animate(with: frames, timePerFrame: timePerFrame)
+    }
+
 	
 	func spawnPlayerEntitySpriteWithTexture() {
-		let player = SKSpriteNode(imageNamed: "Player")
-		player.scale(to: defaultScale)
+		let player = SKSpriteNode(imageNamed: "Galinhaframe1")
+		player.scale(to: CGSize(width: 350, height: 350))
 		player.position = CGPoint(x: playerXSP,
 								  y: playerYSP)
 		player.name = "player"
+
+        let animation = createAnimation(texturePrefix: "Galinhaframe", frameCount: 4, timePerFrame: 0.1)
+
+        player.run(SKAction.repeatForever(animation))
+
 		addChild(player)
 	}
 	
